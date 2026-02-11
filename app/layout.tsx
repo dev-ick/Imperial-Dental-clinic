8d9694825a48f9bb71491bfd9bdc125e35384124
oirere <oirerevictor@gmail.com>
2026-02-11 13:37:05 +0300

    Deleted Unnecessary files

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Enamel Dental Clinic",
+  title: "Imperial Dental Clinic",
   description: "Modern, patient-focused dental care",
 };
 
